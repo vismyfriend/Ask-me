72536636ab054618c8f43431c2802085cefefdf8
Vismyfriend <vismyfriend@yahoo.com>
2023-05-10 19:48:48 +0300

    questions

---
+++ README.md
@@ -0,0 +1,2 @@
+#a game to learn English
+[go to demo](git@github.com:vismyfriend/Ask-me.git)
--- scripts/utils/ques-answ.js
+++ scripts/utils/ques-answ.js
@@ -1,44 +1,159 @@
 const questions = [
     {
         id: 1,
-        ru: "пить пиво",
-        eng: "drink beer"
+        ru: "Как вас зовут?",
+        eng: "What is your name?"
     },
     {
         id: 2,
-        ru: "танцевать",
-        eng: "dance"
+        ru: "Сколько вам лет?",
+        eng: "How old are you?"
     },
     {
         id: 3,
-        ru: "смотреть ютюб",
-        eng: "watch youtube"
+        ru: "Откуда вы?",
+        eng: "Where are you from?"
     },
     {
         id: 4,
-        ru: "читать книги",
-        eng: "read books"
+        ru: "Какого цвета ваши глаза?",
+        eng: "What color are your eyes?"
+    },
+    {
+        id: 5,
+        ru: "	Oops… пока не загрузили перевод… Справитесь сами?	",
+        eng: "	What time do you need to wake up tomorrow?	"
+    },
+    {
+        id: 6,
+        ru: "	Oops… пока не загрузили перевод… Справитесь сами?	",
+        eng: "	Why do people go to Karaoke?	"
+    },
+    {
+        id: 7,
+        ru: "	Oops… пока не загрузили перевод… Справитесь сами?	",
+        eng: "	What music do you like to listen to?	"
+    },
+    {
+        id: 8,
+        ru: "	Oops… пока не загрузили перевод… Справитесь сами?	",
+        eng: "	What song do you want to sing together?	"
+    },
+    {
+        id: 9,
+        ru: "	Oops… пока не загрузили перевод… Справитесь сами?	",
+        eng: "	Why do people study English?	"
+    },
+    {
+        id: 10,
+        ru: "	Oops… пока не загрузили перевод… Справитесь сами?	",
+        eng: "	Why do you study English?	"
+    },
+    {
+        id: 11,
+        ru: "	Oops… пока не загрузили перевод… Справитесь сами?	",
+        eng: "	Why does Vincent go to the GYM?	"
+    },
+    {
+        id: 12,
+        ru: "	Oops… пока не загрузили перевод… Справитесь сами?	",
+        eng: "	Where do you want to travel?	"
+    },
+    {
+        id: 13,
+        ru: "	Oops… пока не загрузили перевод… Справитесь сами?	",
+        eng: "	Where does your best friend want to travel?	"
+    },
+    {
+        id: 14,
+        ru: "	Oops… пока не загрузили перевод… Справитесь сами?	",
+        eng: "	Where do all people go to watch a movie?	"
+    },
+    {
+        id: 15,
+        ru: "	Oops… пока не загрузили перевод… Справитесь сами?	",
+        eng: "	Why does everybody need to work?	"
+    },
+    {
+        id: 16,
+        ru: "	Oops… пока не загрузили перевод… Справитесь сами?	",
+        eng: "	Do you want to dance now?	"
+    },
+    {
+        id: 17,
+        ru: "	Oops… пока не загрузили перевод… Справитесь сами?	",
+        eng: "	What do you want to eat now?	"
+    },
+    {
+        id: 18,
+        ru: "	Oops… пока не загрузили перевод… Справитесь сами?	",
+        eng: "	Where do you want to live when you are 60 years old?	"
+    },
+    {
+        id: 19,
+        ru: "	Oops… пока не загрузили перевод… Справитесь сами?	",
+        eng: "	Does your husband or your wife want to smoke?	"
+    },
+    {
+        id: 20,
+        ru: "	Oops… пока не загрузили перевод… Справитесь сами?	",
+        eng: "	What does your boyfriend or girlfriend like to do?	"
+    },
+    {
+        id: 21,
+        ru: "	Oops… пока не загрузили перевод… Справитесь сами?	",
+        eng: "	What does your boyfriend or girlfriend like to do?	"
+    },
+    {
+        id: 22,
+        ru: "	Oops… пока не загрузили перевод… Справитесь сами?	",
+        eng: "	What does your boyfriend or girlfriend like to do?	"
+    },
+    {
+        id: 23,
+        ru: "	Oops… пока не загрузили перевод… Справитесь сами?	",
+        eng: "	What does your boyfriend or girlfriend like to do?	"
+    },
+    {
+        id: 24,
+        ru: "	Oops… пока не загрузили перевод… Справитесь сами?	",
+        eng: "	What does your boyfriend or girlfriend like to do?	"
+    },
+    {
+        id: 25,
+        ru: "	Oops… пока не загрузили перевод… Справитесь сами?	",
+        eng: "	What does your boyfriend or girlfriend like to do?	"
+    },
+    {
+        id: 26,
+        ru: "	Oops… пока не загрузили перевод… Справитесь сами?	",
+        eng: "	What does your boyfriend or girlfriend like to do?	"
+    },
+    {
+        id: 27,
+        ru: "	Oops… пока не загрузили перевод… Справитесь сами?	",
+        eng: "	What does your boyfriend or girlfriend like to do?	"
     }
 ]
 const answers = [
     {
         id: 101,
-        ru: "иногда",
-        eng: "sometimes"
+        ru: "101 Нажимай на другую колоду",
+        eng: "101 Click another deck"
     },
     {
         id: 102,
-        ru: "после уроков",
-        eng: "after lessons"
+        rru: "102 Нажимай на другую колоду",
+        eng: "102 Click another deck"
     },
     {
         id: 103,
-        ru: "перед уроками",
-        eng: "before lessons"
+        ru: "103 Нажимай на другую колоду",
+        eng: "103 Click another deck"
     },
     {
         id: 104,
-        ru: "когда мне скучно",
-        eng: "when I'm bored"
+        ru: "104 Нажимай на другую колоду",
+        eng: "104 Click another deck"
     }
 ]
